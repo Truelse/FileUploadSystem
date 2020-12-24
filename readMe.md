45c97a85a3883e2e8fd43ef2581c5bbf1a955788
li_wei <li.wei>
2020-12-24 20:24:52 +0800

    add by liwei

---
+++ readMe.md
@@ -0,0 +1,2 @@
+# 2020年12月24日
+    1、新建git私有仓库，地址：https://github.com/Truelse/FileUploadSystem.git
---
+++ src/main/webapp/src/components/common/Homew.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="wrapper">
+        <v-head @my-event="getMyEvent" ref="childrenmode"></v-head>
+        <div class="content-box-homew" :class="{'content-collapse':collapse}">
+            <div class="content">
+                <transition name="move" mode="out-in">
+                    <keep-alive :include="tagsList">
+                        <router-view></router-view>
+                    </keep-alive>
+                </transition>
+                <el-backtop target=".content"></el-backtop>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import vHead from './Headerw.vue';
+import bus from './bus';
+export default {
+    data() {
+        return {
+            tagsList: [],
+            collapse: false,
+        };
+    },
+    components: {
+        vHead,
+    },
+};
+</script>
+
+<style scoped>
+.content-box-homew {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 70px;
+    bottom: 0;
+    padding-bottom: 30px;
+    -webkit-transition: left .3s ease-in-out;
+    transition: left .3s ease-in-out;
+    background: #f0f0f0;
+}
+</style>
