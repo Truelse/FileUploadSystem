9073ff5ea6fdaac198025ac81218b62017d26427
Tom_h <1039650779@qq.com>
2020-12-27 03:40:46 +0800

    新增图表配置修改

--- src/main/webapp/src/components/common/LeftChartArea.vue
+++ src/main/webapp/src/components/common/LeftChartArea.vue
@@ -1,59 +1,67 @@
 <template>
-<div>
-  <dv-border-box-12>
-    <div class="chartsMap">
-      <p>
-        重要文件
-      </P> 
-        <ve-pie :data="chartData"  :extend="chartExtend" height='220px'  ></ve-pie>
-    </div>
-  </dv-border-box-12>
+  <div>
     <dv-border-box-12>
       <div class="chartsMap">
-      <p>
-        一般文件
-      </P> 
-        <ve-pie :data="chartData" :extend="chartExtend" height='220px' ></ve-pie>
-    </div>  
-  </dv-border-box-12>
-</div>  
+        <p>
+          重要文件
+        </P>
+        <ve-pie :data="chartData" :extend="chartExtend" height='220px'></ve-pie>
+      </div>
+    </dv-border-box-12>
+    <dv-border-box-12>
+      <div class="chartsMap">
+        <p>
+          一般文件
+        </P>
+        <ve-pie :data="chartData" :extend="chartExtend" height='220px'></ve-pie>
+      </div>
+    </dv-border-box-12>
+  </div>
 </template>
 
 <script>
-export default {
+  export default {
 
-  name: 'leftchartarea',
-      data () {
+    name: 'leftchartarea',
+    data() {
       this.chartExtend = {
-      legend: {
-      orient : 'vertical',
-        top:50,
-        right:40,
-        textStyle:{
-          color:"#fff"
-        }
-      },
-      series: {
-name: "流量占比分布",
-type: "pie",
-radius : '70%',
-center: ["35%", "100"], 　　　　//这个属性调整图像的位置
-label: { //去除饼图的指示折线
-normal: {
-show: false,
-position: 'inside',
-formatter:"{b}:{d}%"
-},
-},
-},
-    };
+        legend: {
+          orient: 'vertical',
+          top: 50,
+          right: 40,
+          textStyle: {
+            color: "#fff"
+          }
+        },
+        series: {
+          name: "流量占比分布",
+          type: "pie",
+          radius: '70%',
+          center: ["35%", "100"], //这个属性调整图像的位置
+          label: { //去除饼图的指示折线
+            normal: {
+              show: false,
+              position: 'inside',
+              formatter: "{b}:{d}%"
+            },
+          },
+        },
+      };
       return {
         chartData: {
-          columns: ['name', 'number','已完成'],
-          rows: [
-            { 'name': '完成', 'number': 1393, },
-            { 'name': '待审核', 'number': 3530, },
-            { 'name': '已完成', 'number': 3530, },
+          columns: ['name', 'number', '已完成'],
+          rows: [{
+              'name': '完成',
+              'number': 1393,
+            },
+            {
+              'name': '待审核',
+              'number': 3530,
+            },
+            {
+              'name': '已完成',
+              'number': 3530,
+            },
 
           ]
         }
@@ -63,7 +71,7 @@ formatter:"{b}:{d}%"
 </script>
 
 <style lang="less">
-.chartsMap{
+  .chartsMap {
     padding: 20px;
-}
+  }
 </style>
--- src/main/webapp/src/components/page/Main.vue
+++ src/main/webapp/src/components/page/Main.vue
@@ -22,72 +22,78 @@
 </template>
 
 <script>
-import MainHeader from '../common/MainHeader';
-import FilesArea from '../common/FilesArea';
-import LeftFilesArea from '../common/LeftFilesArea'; 
-import LeftChartArea from '../common/LeftChartArea';
-import TableArea from '../common/TableArea';
-import RightChartArea from '../common/RightChartArea';
-import OptionArea from '../common/OptionArea';
-import MapArea from '../common/MapArea';
+  import MainHeader from '../common/MainHeader';
+  import FilesArea from '../common/FilesArea';
+  import LeftFilesArea from '../common/LeftFilesArea';
+  import LeftChartArea from '../common/LeftChartArea';
+  import TableArea from '../common/TableArea';
+  import RightChartArea from '../common/RightChartArea';
+  import OptionArea from '../common/OptionArea';
+  import MapArea from '../common/MapArea';
 
 
-                        
 
-export default {
-  name: 'DataView',
-  components: {
-    MainHeader,
-    FilesArea,
-    LeftFilesArea,
-    LeftChartArea,
-    TableArea,
-    RightChartArea,
-    OptionArea,
-    MapArea
-  },
-  data () {
-    return {}
-  },
-  methods: {}
-}
+
+  export default {
+    name: 'DataView',
+    components: {
+      MainHeader,
+      FilesArea,
+      LeftFilesArea,
+      LeftChartArea,
+      TableArea,
+      RightChartArea,
+      OptionArea,
+      MapArea
+    },
+    data() {
+      return {}
+    },
+    methods: {}
+  }
 </script>
 
 <style lang="less">
-#data-view {
+  #data-view {
     width: 100%;
     height: 100%;
     background-color: #030409;
     color: #fff;
+
     #dv-full-screen-container {
-        background-image: url(../img/bg.png);
-        background-size: 100% 100%;
-        box-shadow: 0 0 3px blue;
-        display: flex;
-        flex-direction: column;
+      background-image: url(../img/bg.png);
+      background-size: 100% 100%;
+      box-shadow: 0 0 3px blue;
+      display: flex;
+      flex-direction: column;
     }
 
     .main-content {
-        display: flex;
-        flex-direction: row;
+      display: flex;
+      flex-direction: row;
     }
+
     .main-left {
-        flex: 3;
-         padding:0 20px;
+      flex: 3;
+      padding: 0 20px;
     }
+
     .main-middle {
-        flex: 6;
-         padding:0 20px;
+      flex: 6;
+      padding: 0 20px;
     }
+
     .main-right {
-        flex:3;
-         padding:0 20px;
+      flex: 3;
+      padding: 0 20px;
     }
-    .dv-border-box-12 .dv-border-svg-container{
+
+    .dv-border-box-12 .dv-border-svg-container {
       width: inherit;
     }
-     .dv-border-box-12{
-       height: inherit;
-     }
-}
+
+    .dv-border-box-12 {
+      height: inherit;
+    }
+  }
 </style>
