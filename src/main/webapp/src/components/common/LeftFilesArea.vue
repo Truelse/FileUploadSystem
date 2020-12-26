e5abb9eaff026804d2834598550314e6c8a7751f
Tom_h <1039650779@qq.com>
2020-12-27 03:58:08 +0800

    文件修改

--- src/main/webapp/src/components/common/LeftFilesArea.vue
+++ src/main/webapp/src/components/common/LeftFilesArea.vue
@@ -7,48 +7,54 @@
           <span class="number">71,876</span>
         </dv-border-box-7>
         <dv-border-box-7 class="file-item">
-                    <p> 待上传</p>
+          <p> 待上传</p>
           <span class="number">71,876</span>
         </dv-border-box-7>
       </div>
       <div class="file-listItems">
-        <dv-border-box-7 class="file-item">       
-             <p> 未审核文件</p>
-          <span class="number" style="color:red">71,876</span></dv-border-box-7>
-        <dv-border-box-7 class="file-item">      
-              <p> 入库文档</p>
-          <span class="number">71,876</span></dv-border-box-7>
+        <dv-border-box-7 class="file-item">
+          <p> 未审核文件</p>
+          <span class="number" style="color:red">71,876</span>
+        </dv-border-box-7>
+        <dv-border-box-7 class="file-item">
+          <p> 入库文档</p>
+          <span class="number">71,876</span>
+        </dv-border-box-7>
       </div>
     </div>
   </dv-border-box-12>
 </template>
 
 <script>
-export default {
-  name: 'leftfilearea'
-}
+  export default {
+    name: 'leftfilearea'
+  }
 </script>
 
 <style lang="less">
-#file-list {
+  #file-list {
     padding: 20px;
+
     .file-listItems {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
     }
+
     .file-listItems:nth-child(2) {
-        margin-top: 20px;
+      margin-top: 20px;
     }
+
     .file-item {
-        width: 150px;
-        height: 100px;
-        box-sizing: border-box;
-        padding: 20px;
-        .number{
-          font-size:32px;
-          margin-top:10px
-        }
+      width: 150px;
+      height: 100px;
+      box-sizing: border-box;
+      padding: 20px;
+
+      .number {
+        font-size: 32px;
+        margin-top: 10px
+      }
     }
-}
+  }
 </style>
--- src/main/webapp/src/components/common/OptionArea.vue
+++ src/main/webapp/src/components/common/OptionArea.vue
@@ -1,82 +1,73 @@
 <template>
-     <dv-border-box-12 >
-       <div class="optionList">
-                 <dv-border-box-2 
-                 
-            style="width: 120px; height: 50px; line-height: 50px; text-align:center;cursor:pointer">
-            <span  class="option" @click="uploadFile()">文件上传 </span>
-            
-          </dv-border-box-2>
-                  <dv-border-box-2                  
+  <dv-border-box-12>
+    <div class="optionList">
+      <dv-border-box-2 style="width: 120px; height: 50px; line-height: 50px; text-align:center;cursor:pointer">
+        <span class="option" @click="uploadFile()">文件上传 </span>
+      </dv-border-box-2>
+      <dv-border-box-2 style="width: 120px; height: 50px; line-height: 50px; text-align:center;cursor:pointer">
+        <span class="option" @click="auditFile()">文件审核 </span>
+      </dv-border-box-2>
+      <dv-border-box-2 style="width: 120px; height: 50px; line-height: 50px; text-align:center;cursor:pointer">
+        <span class="option" @click="issueFile()">任务发布 </span>
+      </dv-border-box-2>
+    </div>
 
-            style="width: 120px; height: 50px; line-height: 50px; text-align:center;cursor:pointer">
-            
-          <span class="option" @click="auditFile()">文件审核 </span>
-
-          </dv-border-box-2>
-                  <dv-border-box-2 
-            style="width: 120px; height: 50px; line-height: 50px; text-align:center;cursor:pointer">
-           <span  class="option" @click="issueFile()">任务发布 </span>
-
-          </dv-border-box-2>
-       </div>  
-
-    </dv-border-box-12>
+  </dv-border-box-12>
 </template>
 <script>
-import bus from '../common/bus';
-export default {
+  import bus from '../common/bus';
+  export default {
     data() {
-        return {
-            collapse: false,
-            fullscreen: false,
-            hidden: false,
-            name: 'linxin',
-            message: 2
-        };
+      return {
+        collapse: false,
+        fullscreen: false,
+        hidden: false,
+        name: 'linxin',
+        message: 2
+      };
     },
     computed: {
-        username() {
-            let username = localStorage.getItem('ms_username');
-            return username ? username : this.name;
-        }
+      username() {
+        let username = localStorage.getItem('ms_username');
+        return username ? username : this.name;
+      }
     },
     methods: {
       //文件上传
-      uploadFile(){
-                 console.log("xx")
+      uploadFile() {
+        console.log("xx")
 
-         this.$router.push('/UploadFile');
-         console.log("xx")
+        this.$router.push('/UploadFile');
+        console.log("xx")
       },
       //文件审核
-      auditFile(){
-         console.log("xx")
-                 this.$router.push('/AuditFile');
+      auditFile() {
+        console.log("xx")
+        this.$router.push('/AuditFile');
 
-          
+
       },
       //任务发布
-      issueFile(){
-                         this.$router.push('/IssueFile');
+      issueFile() {
+        this.$router.push('/IssueFile');
 
       }
     },
     mounted() {
-        if (document.body.clientWidth < 1500) {
-            this.collapseChage();
-        }
+      if (document.body.clientWidth < 1500) {
+        this.collapseChage();
+      }
     }
-};
+  };
 </script>
-<style scoped>
-.optionList{
-       display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-        padding:20px 0;
-        .option{
-          display:block;
-        }
-}
+<style scoped lang="less">
+  .optionList {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    padding: 20px 0;
+    .option {
+      display: block;
+    }
+  }
 </style>
