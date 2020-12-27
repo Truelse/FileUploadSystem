776eafb1cabd7ca1c9c6ced8a86138ce19bbfa90
Tom_h <1039650779@qq.com>
2020-12-28 01:48:55 +0800

    地图配置提交

--- src/main/webapp/src/components/common/MapArea.vue
+++ src/main/webapp/src/components/common/MapArea.vue
@@ -1,69 +1,97 @@
 <template>
   <dv-border-box-12>
-    <div id="map">
-      <ve-map :data="chartData0" :settings="chartSettings22" height='740px' width='600px' style='margin:0px auto;'
-        :grid="grid" :legend-visible='false'></ve-map>
+    <div id="map" style="height:740px; width:900px">
+
     </div>
   </dv-border-box-12>
 </template>
 
 <script>
+  import echart from "echarts"
+  import $ from "jquery"
+  import dataJson from "../../static/jiulongpo.json"
   export default {
     name: 'maparea',
     data() {
-      this.chartSettings22 = {
-        position: 'province/chongqing', //位置  默认china  填写省市 要带province
-        labelMap: {
-          'people': '人口' //修改指标名称
+      return {}
+    },
+
+    mounted() {
+      //定义数量
+      var myData = [
+
+        {
+          name: '谢家湾小学',
+          value: [29.529575,106.480906,90]
         },
-        itemStyle: {
-          normal: { //不选中样式
-            borderColor: '#E6EFFF', //背景颜色
-            areaColor: '#0E7DEB',
-            opacity: 0.2
+      ]
+      var myChart = echart.init(document.getElementById("map"));
+      console.log(dataJson)
+      echart.registerMap('region', dataJson);
+      var option = {  
+           
+        series: [
+          {
+            name: '县级地图',
+            type: 'map',
+            roam: true,
+            left: "center",
+            top: "center",
+            geoIndex: 0,
+            zoom: 1.2, //默认显示级别
+            // scaleLimit:{min:0.1,max:3}, // 缩放级别
+            map: 'region',
+            itemStyle: {
+              color: "#fff",
+              normal: {
+                areaColor: '#45c8ff',
+                borderColor: '#fff',
+                color: "#fff"
+              },
+              emphasis: {
+                areaColor: '#00aeee',
+                color: "#fff"
+              }
+            },
+            // itemStyle:{
+            //     emphasis:{label:{show:true}}
+            // },
+            label: {
+              position: ['50%', '50%'],
+              normal: {
+                show: true,
+                color: "#fff",
+              },
+              color: "#fff",
+              emphasis: {
+                show: false
+              }
+            },
+            data:[12,20]
           },
-          emphasis: { //选中样式
-            areaColor: "#6A9FFF",
-            color: '#fff'
+          {
+            name: '数量', // series名称
+  	        backgroundColor: '#404a59',
+            type: 'scatter',
+            coordinateSystem: 'geo',
+            geoIndex:0,
+            label: {
+              normal: {
+                formatter: '{b}',
+                position: 'right',
+                color: '#F62157', // 标志颜色
+                show: true
+              },
+              emphasis: {
+                show: true
+              }
+            },
+            data: myData // series坐标系类型
           }
-        },
-        label: { //文本设置
-          show: true,
-          fontSize: 8,
-          fontWeight: 300,
-          color: '#fff'
-        },
-      }
-      return {
-        chartData0: {
-          columns: ['位置', 'people'],
-          rows: [{
-              '位置': '南阳市',
-              'people': 123
-            },
-            {
-              '位置': '洛阳市',
-              'people': 1223
-            },
-            {
-              '位置': '周口市',
-              'people': 2123
-            },
-            {
-              '位置': '商丘市',
-              'people': 4123
-            },
-            {
-              '位置': '信阳市',
-              'people': 8123
-            },
-            {
-              '位置': '三门峡市',
-              'people': 8123
-            }
-          ]
-        },
-      }
+        ]
+      };
+      myChart.setOption(option);
+
     }
   }
 </script>
