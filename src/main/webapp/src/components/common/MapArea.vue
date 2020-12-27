fcc87b63990be23f48aa504838ecb7541faa8a0c
Tom_h <1039650779@qq.com>
2020-12-28 02:17:37 +0800

    地图文件配置 及部分数据构造

--- src/main/webapp/src/components/common/MapArea.vue
+++ src/main/webapp/src/components/common/MapArea.vue
@@ -18,77 +18,112 @@
 
     mounted() {
       //定义数量
-      var myData = [
-
+      var myData = [{
+          name: '格力电器',
+          value: [106.45762, 29.507393]
+        },
         {
-          name: '谢家湾小学',
-          value: [29.529575,106.480906,90]
+          name: '谢家湾小学2',
+          value: [106.480906, 29.529575]
+        },
+        {
+          name: '民安华福',
+          value: [106.451943, 29.438499]
+        },
+        {
+          name: '西彭',
+          value: [106.333794, 29.304433]
+        },
+
+      ]
+      //定义值
+      var myValue = [{
+          name: '格力电器',
+          num: 22
+        },
+        {
+          name: '谢家湾小学2',
+          num: 33
+        },
+        {
+          name: '民安华福',
+          num: 44
+        },
+        {
+          name: '西彭',
+          num: 55
         },
       ]
       var myChart = echart.init(document.getElementById("map"));
-      console.log(dataJson)
       echart.registerMap('region', dataJson);
-      var option = {  
-           
-        series: [
-          {
-            name: '县级地图',
-            type: 'map',
-            roam: true,
-            left: "center",
-            top: "center",
-            geoIndex: 0,
-            zoom: 1.2, //默认显示级别
-            // scaleLimit:{min:0.1,max:3}, // 缩放级别
-            map: 'region',
-            itemStyle: {
-              color: "#fff",
-              normal: {
-                areaColor: '#45c8ff',
-                borderColor: '#fff',
-                color: "#fff"
-              },
-              emphasis: {
-                areaColor: '#00aeee',
-                color: "#fff"
-              }
+      var option = {
+        geo: {
+          show: true,
+          map: 'region',
+          nameMap: "region",
+          label: {
+            normal: {
+              show: false
             },
-            // itemStyle:{
-            //     emphasis:{label:{show:true}}
-            // },
-            label: {
-              position: ['50%', '50%'],
-              normal: {
-                show: true,
-                color: "#fff",
-              },
-              color: "#fff",
-              emphasis: {
-                show: false
-              }
-            },
-            data:[12,20]
+            emphasis: {
+              show: false
+            }
           },
-          {
-            name: '数量', // series名称
-  	        backgroundColor: '#404a59',
-            type: 'scatter',
-            coordinateSystem: 'geo',
-            geoIndex:0,
-            label: {
-              normal: {
-                formatter: '{b}',
-                position: 'right',
-                color: '#F62157', // 标志颜色
-                show: true
-              },
-              emphasis: {
-                show: true
-              }
+          roam: true,
+          itemStyle: {
+            color: "#fff",
+            show:false,
+            normal: {
+              areaColor: '#00aeee',
+              borderColor: '#fff',
+              color: "#fff",
+               show:false
             },
-            data: myData // series坐标系类型
-          }
-        ]
+            emphasis: {
+              areaColor: '#00aeee',
+              color: "#fff",
+              show:false
+            }
+          },
+          label: {
+            position: ['50%', '50%'],
+            normal: {
+              show: true,
+              color: "#fff",
+            },
+            color: "#fff",
+            emphasis: {
+              show: false
+            }
+          },
+          zoom: 1.2
+        },
+        series: [{
+          name: '数量', // series名称
+          backgroundColor: '#404a59',
+          type: 'scatter',
+          coordinateSystem: 'geo',
+          geoIndex: 0,
+          label: {
+            normal: {
+              formatter: function (b) {
+                console.log(b.name)
+                for (var i = 0; i < myValue.length; i++) {
+                  if (b.name == myValue[i].name) {
+                    return b.name + myValue[i].num + '个'
+                  }
+                }
+              },
+              position: 'right',
+              color: '#F62157', // 标志颜色
+              show: true
+            },
+            emphasis: {
+              show: true
+            }
+          },
+          data: myData // series坐标系类型
+        }]
       };
       myChart.setOption(option);
 
